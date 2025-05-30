50967bb1dbfc1927be2bd290498fcb75605ed4e8
MMeza91 <matias.mezag@usach.cl>
2025-05-29 22:43:23 -0400

    Se termina de recordatorios.py

--- recordatorios.py
+++ recordatorios.py
@@ -4,5 +4,11 @@ recordatorios = [['2021-01-01', "11:00", "Levantarse y ejercitar"],
  ['2021-09-18', "16:00", "Ramadas"],
  ['2021-12-25', "00:00", "Navidad"]]
 
+recordatorios.insert(1,['2021-02-02', '06:00', 'Empezar el Año'])
+recordatorios[3][0]='2021-07-16'
+recordatorios.pop(2)
+recordatorios.insert(-1,['2021-12-25', '22:00', 'Cena de Navidad'])
+recordatorios.append(['2021-12-31', '22:00', 'Cena de Año Nuevo'])
+
 # Output
 print(recordatorios)
